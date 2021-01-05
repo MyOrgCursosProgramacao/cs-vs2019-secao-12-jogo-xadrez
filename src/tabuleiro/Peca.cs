28699b7555cd22bcddaf21174ccaa27a99d948c7
Diego <alvarodiego@gmail.com>
2021-01-05 14:49:30 -0300

    Classe peca

---
+++ src/tabuleiro/Peca.cs
@@ -0,0 +1,22 @@
+﻿namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; }
+        public int QtdMovimentos { get; protected set; }
+        public Tabuleiro Tabuleiro { get; protected set; }
+
+        public Peca()
+        {
+        }
+
+        public Peca(Cor cor, Tabuleiro tabuleiro)
+        {
+            Posicao = null;
+            Cor = cor;
+            Tabuleiro = tabuleiro;
+            QtdMovimentos = 0;
+        }
+    }
+}

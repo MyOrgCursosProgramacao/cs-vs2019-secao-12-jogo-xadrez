16fcd32e697488ea5086bb762baf4a146eedc679
Diego <alvarodiego@gmail.com>
2021-01-05 16:57:07 -0300

    Criada classe de posição no campo do xadrez

---
+++ src/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xadrez
+{
+    class PosicaoXadrez
+    {
+        public int Linha { get; set; }
+        public char Coluna { get; set; }
+
+        public PosicaoXadrez(int linha, char coluna)
+        {
+            Linha = linha;
+            Coluna = coluna;
+        }
+
+        public override string ToString()
+        {
+            return "" + Linha + Coluna;
+        }
+    }
+}

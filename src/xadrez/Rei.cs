801b8f68be22e7352116f80784ecc78cd4fc5529
Diego <alvarodiego@gmail.com>
2021-01-07 19:58:34 -0300

    Adicionado o método de movimentosPossiveis

--- src/xadrez/Rei.cs
+++ src/xadrez/Rei.cs
@@ -9,6 +9,32 @@ namespace xadrez
 
         }
 
+        public override bool[,] MovimentosPossiveis()
+        {
+            bool[,] mat = new bool[Tabuleiro.Linhas, Tabuleiro.Colunas];
+
+            Posicao posicaoAux = new Posicao(0, 0);
+
+            for (int i = 0; i < Tabuleiro.Linhas; i++)
+            {
+                for (int j = 0; j < Tabuleiro.Colunas; j++)
+                {
+                    posicaoAux.DefinirPosicao(i, j);
+                    if (posicaoAux != Posicao)
+                    {
+                        if (Tabuleiro.PosicaoValida(posicaoAux) && PodeMover(posicaoAux))
+                        {
+                            if ((System.Math.Abs(i - Posicao.Linha)) <= 1 && (System.Math.Abs(j - Posicao.Coluna) <= 1))
+                            {
+                                mat[i, j] = true;
+                            }
+                        }
+                    }
+                }
+            }
+            return mat;
+        }
+
         public override string ToString()
         {
             return "R";

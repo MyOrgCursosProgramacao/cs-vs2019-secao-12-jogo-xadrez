e640e0427872db5a69c5343aa5e7da68c632c196
Diego <alvarodiego@gmail.com>
2021-01-07 19:59:55 -0300

    Teste iniciais de movimento com sucesso

--- src/Program.cs
+++ src/Program.cs
@@ -9,20 +9,37 @@ namespace src
         static void Main(string[] args)
         {
             PartidaDeXadrez partida = new PartidaDeXadrez();
+            bool[,] posicoesPossiveis = new bool[partida.Tabuleiro.Linhas, partida.Tabuleiro.Colunas];
             try
             {
                 while (!partida.terminada)
                 {
                     Console.Clear();
                     Console.WriteLine(partida.Turno + " turno");
-                    Tela.ImprimirTabuleiro(partida.Tabuleiro);
+
+
+                    Tela.ImprimirTabuleiro(partida.Tabuleiro, posicoesPossiveis);
 
                     Console.Write("Origem: ");
                     Posicao origem = Tela.LerPosicaoXadrez().ToPosicao();
+
+                    Console.Clear();
+                    posicoesPossiveis = partida.Tabuleiro.GetPeca(origem).MovimentosPossiveis();
+                    Console.WriteLine(partida.Turno + " turno");
+                    Tela.ImprimirTabuleiro(partida.Tabuleiro, posicoesPossiveis);
+
                     Console.Write("Destino: ");
                     Posicao destino = Tela.LerPosicaoXadrez().ToPosicao();
 
-                    partida.ExecutaMovimento(origem, destino);
+                    for (int i = 0; i < posicoesPossiveis.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < posicoesPossiveis.GetLength(1); j++)
+                        {
+                            posicoesPossiveis[i, j] = false;
+                        }
+                    }
+
+                        partida.ExecutaMovimento(origem, destino);
                 }
             }
             catch (TabuleiroException e)
@@ -31,7 +48,7 @@ namespace src
             }
             finally
             {
-                Tela.ImprimirTabuleiro(partida.Tabuleiro);
+                Tela.ImprimirTabuleiro(partida.Tabuleiro, posicoesPossiveis);
                 Console.WriteLine();
             }
 
